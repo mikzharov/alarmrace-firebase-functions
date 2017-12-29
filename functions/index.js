70c801b054714b6d85fefd905e9dc20a4dd198c3
Misha Zharov <mishazharov1@gmail.com>
2017-12-28 23:03:07 -0500

    Working on it

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,30 @@
+'use strict';
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const express = require('express');
+const cors = require('cors')({origin: true});
+const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+const validateUserToken = (req, res, next) => {
+  if (!req.headers.authorization) {
+    res.status(403).send('Unauthorized');
+    return;
+  }
+
+  let idToken;
+  if (req.headers.authorization) {
+    idToken = req.headers.authorization;
+  }
+  
+  admin.auth().verifyIdToken(idToken).then(decodedIdToken => {
+    req.user = decodedIdToken;
+    next();
+  }).catch(error => {
+    res.status(403).send('Unauthorized');
+  });
+};
+
+app.use(validateUserToken);
+app.use(cors);
