562a5dd17a2f68a29e50fca5e53a2d2aa94f99b7
Misha Zharov <mishazharov1@gmail.com>
2017-12-28 21:41:48 -0500

    First commit

---
+++ functions/index.js
@@ -0,0 +1,10 @@
+'use strict';
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const express = require('express');
+const cors = require('cors')({origin: true});
+const app = express();
+
+
+app.use(cors);
